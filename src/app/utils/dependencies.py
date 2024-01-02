9a21c0f6d25756ea7a22dbdb379786aea2c4bd4b
longqua69 <longqua69@gmail.com>
2024-01-02 22:31:27 +0700

    created database dependencies and handlers

---
+++ src/app/utils/dependencies.py
@@ -0,0 +1,10 @@
+from fastapi import Depends
+
+from app.database.settings import local_session
+
+def database_session():
+    session = local_session()
+    try:
+        yield session
+    finally:
+        session.close()
